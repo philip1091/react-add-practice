d1bcb19f484bab869b2cf43ad1bbffdf2dc65e0a
Jean Philippe Hicintuka <jeanphilh@outlook.com>
2020-04-28 19:05:08 +0800

    my first react app

---
+++ src/components/app.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "61VzGiRUQp3pZ8omne"
+    }
+  }
+
+  search = (query) => {
+    giphy('Ezqyc6nMQslnC3gCPha1yFVhxbJheR5j').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search}/>
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+
+          <GifList gifs={this.state.gifs} />
+
+        </div>
+      </div>
+    )
+  }
+}
+
+  export default App;
